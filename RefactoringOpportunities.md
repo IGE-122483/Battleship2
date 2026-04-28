f5ac226420570082155ee213aa2d77298c08d230
Filipe Santos <flsso1@iscte-iul.pt>
2026-04-28 21:37:54 +0100

    Extract Method

---
+++ RefactoringOpportunities.md
@@ -0,0 +1,6 @@
+| Local | Cheiro no código | Refabricação | Nº aluno |
+| Pdf::exportarJogadas | Long Method | Extract Method | 122471 |
+| Pdf::adicionarCabecalho | Data Flow / Redundancy | Introduce Variable | 122471 |
+| Pdf::adicionarCabecalho | Data Flow / Redundancy | Inline Variable | 122471 |
+| Pdf::adicionarCabecalho; Pdf::adicionarJogadasAoDocumento | Object-Oriented Design | Move Method | 122471 |
+| PdfContentWriter::adicionarJogadasSeExistirem | Expressions & Conditionals | Decompose Conditional | 122471 |
--- src/main/java/battleship/Pdf.java
+++ src/main/java/battleship/Pdf.java
@@ -1,7 +1,6 @@
 package battleship;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.FileOutputStream;
@@ -10,27 +9,36 @@ import java.util.List;
 public class Pdf {
 
     public static void exportarJogadas(List<String> jogadas, String nomeFicheiro) {
-
         try {
-
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream(nomeFicheiro));
-
-            document.open();
-
-            document.add(new Paragraph("Historico de Jogadas"));
-            document.add(new Paragraph(" "));
-
-            for (String jogada : jogadas) {
-                document.add(new Paragraph(jogada));
-            }
-
-            document.close();
-
+            Document document = criarDocumento(nomeFicheiro);
+            adicionarCabecalho(document);
+            adicionarJogadasAoDocumento(document, jogadas);
+            fecharDocumento(document);
             System.out.println("PDF criado com sucesso!");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Document criarDocumento(String nomeFicheiro) throws Exception {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(nomeFicheiro));
+        document.open();
+        return document;
+    }
+
+    private static void adicionarCabecalho(Document document) throws Exception {
+        document.add(new com.lowagie.text.Paragraph("Historico de Jogadas"));
+        document.add(new com.lowagie.text.Paragraph(" "));
+    }
+
+    private static void adicionarJogadasAoDocumento(Document document, List<String> jogadas) throws Exception {
+        for (String jogada : jogadas) {
+            document.add(new com.lowagie.text.Paragraph(jogada));
+        }
+    }
+
+    private static void fecharDocumento(Document document) {
+        document.close();
     }
 }
