15c10d042cc2ef868c7b7530e8b4885022d6a280
Filipe Santos <flsso1@iscte-iul.pt>
2026-04-29 12:44:06 +0100

    Move Method

--- src/main/java/battleship/Pdf.java
+++ src/main/java/battleship/Pdf.java
@@ -2,7 +2,6 @@ package battleship;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
@@ -14,8 +13,8 @@ public class Pdf {
     public static void exportarJogadas(List<String> jogadas, String nomeFicheiro) {
         try {
             Document document = criarDocumento(nomeFicheiro);
-            adicionarCabecalho(document);
-            adicionarJogadasAoDocumento(document, jogadas);
+            PdfContentWriter contentWriter = new PdfContentWriter();
+            contentWriter.preencherDocumento(document, jogadas);
             fecharDocumento(document);
             System.out.println("PDF criado com sucesso!");
         } catch (DocumentException | FileNotFoundException e) {
@@ -28,19 +27,6 @@ public class Pdf {
         PdfWriter.getInstance(document, new FileOutputStream(nomeFicheiro));
         document.open();
         return document;
-    }
-
-    private static void adicionarCabecalho(Document document) throws DocumentException {
-        String titulo = "Historico de Jogadas";
-
-        document.add(new Paragraph(titulo));
-        document.add(new Paragraph(" "));
-    }
-
-    private static void adicionarJogadasAoDocumento(Document document, List<String> jogadas) throws DocumentException {
-        for (String jogada : jogadas) {
-            document.add(new Paragraph(jogada));
-        }
     }
 
     private static void fecharDocumento(Document document) {
